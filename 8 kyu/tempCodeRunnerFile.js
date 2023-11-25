417fd7e3b3b3bf782ec2412d045b8561ffe098f8
Lesia Soloviova <solovyova.lesya@gmail.com>
2023-11-25 23:21:52 +0200

    tasks added

--- 8 kyu/tempCodeRunnerFile.js
+++ 8 kyu/tempCodeRunnerFile.js
@@ -1,29 +1,17 @@
-function getLength(arr) {
-  //return length of arr
-  return arr.length;
-}
-function getFirst(arr) {
-  //return the first element of arr
-  return arr[0];
-}
-function getLast(arr) {
-  //return the last element of arr
-  return arr[arr.length - 1];
-}
-function pushElement(arr) {
-  var el = 1;
-  //push el to arr
-  arr.push(el);
-  return arr;
-}
-function popElement(arr) {
-  //pop an element from arr
-  arr.pop();
-  return arr;
+
+function padIt(str, n) {
+  //coding here
+  let num = 1;
+  const symbolToAdd = "*";
+  do {
+    if (num % 2 === 0) {
+      str = str + symbolToAdd;
+    } else {
+      str = symbolToAdd + str;
+    }
+    num++;
+  } while (num <= n);
+  return str;
 }
 
-console.log(getLength([1, 2, 3]));
-console.log(getFirst([1, 2, 3]));
-console.log(getLast([1, 2, 3]));
-console.log(pushElement([1, 2, 3]));
-console.log(popElement([1, 2, 3]));
+(padIt("a", 1));

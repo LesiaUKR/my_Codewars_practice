3ddbf00bb4630181126c67cdd68867bf780ef359
Lesia Soloviova <solovyova.lesya@gmail.com>
2023-12-28 23:44:49 +0200

    feat: solve task 20

--- 8 kyu/20_Methods of String object--charAt() charCodeAt() and fromCharCode().js
+++ 8 kyu/20_Methods of String object--charAt() charCodeAt() and fromCharCode().js
@@ -169,21 +169,42 @@
 
 function topSecret(str) {
   const shift = 3;
+  const totalAlphabetLength = 26;
   let chars = str.split("");
   let decodedStr = "";
-  console.log(chars);
-  for (let i = 0; i < chars.length; i++) {
-    let currentUnicode = chars[i].charCodeAt();
-    let previousUnicode = currentUnicode - shift;
-    console.log("currentUnicode", currentUnicode);
-    console.log("previousUnicode", previousUnicode);
-    decodedStr = String.fromCharCode();
-    console.log("decodedStr", decodedStr);
-  }
-}
-console.log(topSecret("Pb qdph lv Mrkq")); // "My name is John";
-console.log(topSecret("wklv lv dq hadpsoh")); // "this is an example";
-console.log(topSecret("Khoor Zruog!")); // "Hello World!";
 
-console.log(topSecret("A"));
-console.log("X".charCodeAt());
+  console.log(chars);
+
+  for (let i = 0; i < chars.length; i++) {
+    const currentChar = chars[i];
+    const isUpperCase = chars[i] === chars[i].toUpperCase();
+
+    console.log(isUpperCase);
+
+    if (/^[A-Za-z\n]$/.test(currentChar)) {
+      const baseCharCode = isUpperCase ? "A".charCodeAt(0) : "a".charCodeAt(0);
+      let currentUnicode = currentChar.charCodeAt();
+      console.log("currentUnicode", currentUnicode);
+      decodedUnicode =
+        ((currentUnicode - baseCharCode - shift + totalAlphabetLength) % 26) +
+        baseCharCode;
+      console.log("decodedUnicode", decodedUnicode);
+      decodedStr += String.fromCharCode(decodedUnicode);
+    } else {
+      decodedStr += currentChar;
+    }
+  }
+  return decodedStr;
+}
+
+// console.log(topSecret("B"));
+// console.log("Y".charCodeAt());
+
+//console.log(topSecret("Pb qdph lv Mrkq")); // "My name is John";
+// console.log(topSecret("wklv lv dq hadpsoh")); // "this is an example";
+// console.log(topSecret("Khoor Zruog!")); // "Hello World!";
+console.log(
+  topSecret(
+    "Wrs vhfuhw ilohv: Qr. 2478 Vxshu djhqw LIFOBS, rq d gdun qljkw, vwroh wkh Dssoh fruh iurp Qdwlrqdo Pxvhxp, fdxvlqj $100 ploolrq lq gdpdjh."
+  )
+);

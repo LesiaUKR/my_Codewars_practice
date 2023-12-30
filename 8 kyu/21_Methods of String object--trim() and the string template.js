5f0b1abb66b2cf27e1c54963b00a90d19196c63d
Lesia Soloviova <solovyova.lesya@gmail.com>
2023-12-30 23:53:41 +0200

    feat: solve task 22

--- 8 kyu/21_Methods of String object--trim() and the string template.js
+++ 8 kyu/21_Methods of String object--trim() and the string template.js
@@ -1,3 +1,5 @@
+/******** Task Solution 1 ********/
+
 function fiveLine(s) {
   s = s.trim();
 
@@ -13,3 +15,16 @@ function fiveLine(s) {
 // Test cases
 console.log(fiveLine("  a"));
 console.log(fiveLine("  xy "));
+
+/******** Task Solution 2 ********/
+
+function fiveLine(s) {
+  s = s.trim();
+  return `${s}\n${s.repeat(2)}\n${s.repeat(3)}\n${s.repeat(4)}\n${s.repeat(5)}`;
+}
+
+/******** Task Solution 3 ********/
+
+function fiveLine(s) {
+  return [1, 2, 3, 4, 5].map((n) => s.trim().repeat(n)).join("\n");
+}

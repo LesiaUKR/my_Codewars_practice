447e0672178e7ec63ca56c801d9f3144b82100f5
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-01-17 22:31:01 +0200

    feat: task 30 description added

--- 7 kuy/tempCodeRunnerFile.js
+++ 7 kuy/tempCodeRunnerFile.js
@@ -1,26 +1,9 @@
-function mirrorImage(arr) {
-  let newArr = [];
-  arr.some((currentNumber, i) => {
-    const nextNumber = arr[i + 1];
-    const reversedNumber = parseInt(
-      String(nextNumber).split("").reverse().join(""),
-      10
-    );
-    console.log("currentNumber", currentNumber);
-    console.log("nextNumber", nextNumber);
-    console.log("reversedNumber", reversedNumber);
-    if (currentNumber === reversedNumber) {
-      console.log("if currentNumber", currentNumber);
-      console.log("if nextNumber", nextNumber);
-      newArr.push(currentNumber, nextNumber);
-      return true;
-      // newArr.push(currentNumber);
-      // newArr.push(nextNumber);
-      // console.log(newArr);
-    } else return false;
-  });
-  return newArr;
+function getIntervalArray(start, end) {
+  const arr = [];
+  for (let i = start; i <= end; i += 1) {
+    console.log("i", i);
+    arr.push(i);
+  }
+  return arr;
 }
-
-// console.log(mirrorImage([11, 22, 33, 33, 22, 11])); //should return [33,33]
-console.log(mirrorImage([454, 86, 57, 75, 16, 88]));
+console.log(getIntervalArray(1, 5));

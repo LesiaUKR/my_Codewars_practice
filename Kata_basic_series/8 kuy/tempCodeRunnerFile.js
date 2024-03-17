b8be9b88aada28a4f66f16eb806681bae394348d
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-03-17 21:28:52 +0200

    feat: solve kata 39

--- Kata_basic_series/8 kuy/tempCodeRunnerFile.js
+++ Kata_basic_series/8 kuy/tempCodeRunnerFile.js
@@ -1,11 +1,24 @@
-function highAndLow(numbers) {
-  const arrayOfNumbers = numbers.split(" ").map(Number);
-  console.log("arrayOfNumbers", arrayOfNumbers);
-   const maxNumber = Math.max(...arrayOfNumbers);
-   console.log("maxNumber", maxNumber);
-   const minNumber = Math.min(...arrayOfNumbers);
-   console.log('minNumber', minNumber)
-   return `${maxNumber} ${minNumber}`;
+function points(games) {
+  let totalPoints = 0;
+  console.log("totalPoints", totalPoints);
+  for (let i = 0; i < games.length; i++) {
+    const match = games[i].split(":");
+    console.log("match", match);
+    console.log("match[0]", match[0]);
+    console.log("match[1]", match[1]);
+    console.log("totalPoints", totalPoints);
+    if (match[0] > match[1]) {
+      totalPoints += 3;
+      console.log("totalPoints", totalPoints);
+    } else if (match[0] === match[1]) {
+      totalPoints += 1;
+    }
+    totalPoints += 0;
+  }
+
+  return totalPoints;
 }
 
-console.log('highAndLow("1 2 3 4 5")', highAndLow("1 2 3 4 5"));
+console.log(
+  points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"])
+);
